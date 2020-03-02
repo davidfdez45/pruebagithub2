022696398ca1afb12c27f4f5de87dac7d49f4fb2
davidfdez45 <61683064+davidfdez45@users.noreply.github.com>
2020-03-02 10:45:56 +0100

    añadido comentario

--- app/src/main/java/com/example/pruebagithub2/MainActivity.java
+++ app/src/main/java/com/example/pruebagithub2/MainActivity.java
@@ -12,6 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //aaaa
         //segundo comentario
-        //nandsf
+        //nandsf dfghjk
     }
 }
